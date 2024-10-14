26a5c076a3278eb0f41ee3c0c277380818875faa
kuromon11 <yoshikikuro712@gmail.com>
2024-10-14 14:56:46 +0900

    feat: #2 フッターを作成

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray text-black py-2">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 My Money Note. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
