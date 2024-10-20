5e84488c935aff0a0bfc247172fa2b7875c04935
kuromon11 <yoshikikuro712@gmail.com>
2024-10-20 16:02:29 +0900

    chore: #5 コード整形

--- src/components/InputFormModal.tsx
+++ src/components/InputFormModal.tsx
@@ -49,95 +49,95 @@ const InputFormModal: React.FC<Props> = (props) => {
           &times;
         </button>
         <h2 className="text-xl font-semibold mb-4">収支登録</h2>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              収入/支出
-            </label>
-            <div className="flex items-center">
-              <input
-                type="radio"
-                id="income"
-                name="transactionType"
-                value="income"
-                className="mr-2"
-                checked={balanceType === 'income'}
-                onChange={(e) => changeBalanceType(e.target.value)}
-              />
-              <label htmlFor="income" className="mr-4">
-                収入(+)
-              </label>
-              <input
-                type="radio"
-                id="expense"
-                name="transactionType"
-                value="expense"
-                className="mr-2"
-                checked={balanceType === 'expense'}
-                onChange={(e) => changeBalanceType(e.target.value)}
-              />
-              <label htmlFor="expense">支出(-)</label>
-            </div>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              日付
-            </label>
-            <DatePicker
-              locale="ja"
-              dateFormat="yyyy/MM/dd"
-              dateFormatCalendar="yyyy年 MM月"
-              selected={date ? new Date(date) : null}
-              onChange={(date) => changeDate(date)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            収入/支出
+          </label>
+          <div className="flex items-center">
+            <input
+              type="radio"
+              id="income"
+              name="transactionType"
+              value="income"
+              className="mr-2"
+              checked={balanceType === 'income'}
+              onChange={(e) => changeBalanceType(e.target.value)}
             />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="amount"
-            >
-              金額（円）
+            <label htmlFor="income" className="mr-4">
+              収入(+)
             </label>
             <input
-              type="text"
-              id="amount"
-              inputMode="numeric"
-              value={amount}
-              onChange={(e) => changeAmount(Number(e.target.value))}
-              className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="980"
+              type="radio"
+              id="expense"
+              name="transactionType"
+              value="expense"
+              className="mr-2"
+              checked={balanceType === 'expense'}
+              onChange={(e) => changeBalanceType(e.target.value)}
             />
+            <label htmlFor="expense">支出(-)</label>
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="category"
-            >
-              内容
-            </label>
-            <input
-              type="text"
-              id="category"
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            日付
+          </label>
+          <DatePicker
+            locale="ja"
+            dateFormat="yyyy/MM/dd"
+            dateFormatCalendar="yyyy年 MM月"
+            selected={date ? new Date(date) : null}
+            onChange={(date) => changeDate(date)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="amount"
+          >
+            金額（円）
+          </label>
+          <input
+            type="text"
+            id="amount"
+            inputMode="numeric"
+            value={amount}
+            onChange={(e) => changeAmount(Number(e.target.value))}
+            className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="980"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="category"
+          >
+            内容
+          </label>
+          <input
+            type="text"
+            id="category"
             value={item}
             onChange={(e) => changeItem(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="例：コンビニ、ランチ代、電気代、給与"
-            />
-          </div>
-          <div className="flex items-center justify-end mt-8">
-            <button
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="例：コンビニ、ランチ代、電気代、給与"
+          />
+        </div>
+        <div className="flex items-center justify-end mt-8">
+          <button
             onClick={async () => {
               const params = { id: generateRandomID(), ...payload };
               await api('POST /data', params);
-                close();
-              }}
-              className="w-full bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:text-tahiti disabled:text-tahiti disabled:opacity-50"
-              disabled={isDisabled}
-            >
-              <MdOutlineTaskAlt className="inline-block" />
-              <span className="ml-1">確定</span>
-            </button>
-          </div>
+              close();
+            }}
+            className="w-full bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:text-tahiti disabled:text-tahiti disabled:opacity-50"
+            disabled={isDisabled}
+          >
+            <MdOutlineTaskAlt className="inline-block" />
+            <span className="ml-1">確定</span>
+          </button>
+        </div>
       </div>
     </div>
   );
