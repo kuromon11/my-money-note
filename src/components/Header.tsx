b08b233aaf441b142ca6cf7cc3f4d10ea758f8e0
kuromon11 <yoshikikuro712@gmail.com>
2024-10-20 19:35:05 +0900

    feat: #6 ヘッダー右上のリンクより、ホームと履歴の画面切り替えを行えるよう修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-import { MdHistory } from 'react-icons/md';
+import { MdHistory, MdHome } from 'react-icons/md';
 
 const Header: React.FC = () => {
+  const pathname = useLocation().pathname;
+  const path = pathname === '/' ? 'history' : '/';
+  const icon = pathname === '/' ? <MdHistory /> : <MdHome />;
+  const linkText = pathname === '/' ? '履歴' : 'ホームに戻る';
   return (
     <header className="bg-blue text-white p-4 flex">
       <h1 className="text-xl font-bold">My Money Note</h1>
-      <button className="bg-blue text-white px-2 py-1 rounded ml-auto hover:text-tahiti flex items-center">
-        <MdHistory />
-        <span className="ml-1">履歴</span>
+      <button className="bg-blue text-white rounded ml-auto hover:text-tahiti">
+        <Link className="px-2 py-1 flex items-center" to={path}>
+          {icon}
+          <span className="ml-1">{linkText}</span>
+        </Link>
       </button>
     </header>
   );
